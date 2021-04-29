94000566d5627fc86df68a4dbfcc468299532a3d
name <prampreethanyalak@kkumail.com>
2021-04-29 23:07:20 +0700

    'login'

--- auth.js
+++ auth.js
@@ -1,61 +1,83 @@
-const authSwitchLinks = document.querySelectorAll('.switch');
-const authModals = document.querySelectorAll('.auth .modal');
-const authWrapper = document.querySelector('.auth');
-const registerForm = document.querySelector('.register');
-const loginForm = document.querySelector('.login');
-const signOut = document.querySelector('.sign-out');
+// const authSwitchLinks = document.querySelectorAll('.switch');
+// const authModals = document.querySelectorAll('.auth .modal');
+// const authWrapper = document.querySelector('.auth');
+// const registerForm = document.querySelector('.register');
+// const loginForm = document.querySelector('.login');
+const loginFormWithGmail = document.querySelector('.loginWithGmail');
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+// console.log('signout>>>',signOut)
+// const firebase = require('firebase')
+
+
 
 // toggle auth modals
-authSwitchLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    authModals.forEach(modal => modal.classList.toggle('active'));
+
+const loginWithGmail = () => {
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => { 
+    window.location = 'newCostsplit.html'
+   
+  }).catch((error) => {
+    loginFormWithGmail.querySelector('.error').textContent = error.message;
   });
-});
+}
 
-// register form
-registerForm.addEventListener('submit', (e) => {
-e.preventDefault();
-const email = registerForm.email.value;
-const password = registerForm.password.value;
-firebase.auth().createUserWithEmailAndPassword(email,password)
-.then(user => {
-    console.log('registered', user);
-    registerForm.reset();
-})
-.catch((error) => {
-    registerForm.querySelector('.error').textContent = error.message;
-});
-});
 
-// login form
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  const email = loginForm.email.value;
-  const password = loginForm.password.value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => {
-      console.log('logged in', user);
-      loginForm.reset();
-    })
-    .catch(error => {
-      loginForm.querySelector('.error').textContent = error.message;
-    });
-});
-
-// sign out
-// signOut.addEventListener('click', () => {
-//   firebase.auth().signOut()
-//     .then(() => console.log('signed out'));
+// authSwitchLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     authModals.forEach(modal => modal.classList.toggle('active'));
+//   });
 // });
 
+// register form
+// registerForm.addEventListener('submit', (e) => {
+// e.preventDefault();
+// const email = registerForm.email.value;
+// const password = registerForm.password.value;
+// firebase.auth().createUserWithEmailAndPassword(email,password)
+// .then(user => {
+//     console.log('registered', user);
+//     registerForm.reset();
+// })
+// .catch((error) => {
+//     registerForm.querySelector('.error').textContent = error.message;
+// });
+// });
+
+// login form
+// loginForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+  
+//   const email = loginForm.email.value;
+//   const password = loginForm.password.value;
+//   firebase.auth().signInWithEmailAndPassword(email, password)
+//     .then(user => {
+//       console.log('logged in', user);
+//       loginForm.reset();
+//     })
+//     .catch(error => {
+//       loginForm.querySelector('.error').textContent = error.message;
+//     });
+// });
+
+// login form with gmail
+loginFormWithGmail.addEventListener('click', loginWithGmail);
+
 // auth listener
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
+// firebase.auth().onAuthStateChanged(user => {
+//   console.log('run')
+//   if (user) {
+//     console.log(user)
+//     window.location = 'newCostsplit.html'
     // authWrapper.classList.remove('open');
     // authModals.forEach(modal => modal.classList.remove('active'));
-  } else {
-    authWrapper.classList.add('open');
-    authModals[0].classList.add('active');
-  }
-});
+  // } else {
+  //   console.log('is not log in')
+    // authWrapper.classList.add('open');
+    // authModals[0].classList.add('active');
+    // window.location = 'login.html'
+  // }
+// });
--- script.js
+++ script.js
@@ -2,33 +2,45 @@ const peoplename = document.getElementById("peoplename");
 let numPeople = 0;
 let personvalue = document.getElementById("price_person");
 let peoplesize = document.getElementById("peoplesize");
+const signOut = document.querySelector("#sign-out");
 
-function addName(){
-    if(typename.value != ""){
-        numPeople++;
-        peoplesize.innerText = numPeople;
-        MyNumberType();
-        const nameItem = document.createElement("li");
-        let name = typename.oninput;
-        nameItem.innerHTML = typename.value;
-        peoplename.appendChild(nameItem);
-        typename.value = "";
-    }
+function addName() {
+  if (typename.value != "") {
+    numPeople++;
+    peoplesize.innerText = numPeople;
+    MyNumberType();
+    const nameItem = document.createElement("li");
+    let name = typename.oninput;
+    nameItem.innerHTML = typename.value;
+    peoplename.appendChild(nameItem);
+    typename.value = "";
+  }
 }
 
 function MyNumberType() {
-    var total_price = parseInt(document.getElementById("total_price").value);
-    if (numPeople != 0 && total_price>0) {
-        var personPrice = total_price/numPeople;
-        personvalue.innerText = personPrice;
-    } else {
-        personvalue.innerText = 0;
-    }
+  var total_price = parseInt(document.getElementById("total_price").value);
+  if (numPeople != 0 && total_price > 0) {
+    var personPrice = total_price / numPeople;
+    personvalue.innerText = personPrice;
+  } else {
+    personvalue.innerText = 0;
+  }
 }
 
 function ResetButton() {
-    peoplesize.innerText = 0;
-    var total_price = parseInt(document.getElementById("total_price").value);
-    total_price.innerText = 0;
-
+  peoplesize.innerText = 0;
+  var total_price = parseInt(document.getElementById("total_price").value);
+  total_price.innerText = 0;
 }
+
+// sign out
+// console.log(signOut)
+signOut.addEventListener("click", () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log("signed out");
+      window.location = 'login.html';
+    });
+});
