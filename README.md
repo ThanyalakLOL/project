cc920d2e3a9e509d3cc5eb2be414d26e85ae6dfa
name <prampreethanyalak@kkumail.com>
2021-04-30 18:29:53 +0700

    respon

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Project
-1. Member contribution: Who does what and for how long
-Thanyalak 162-9 :หน้า login และผูก information firebase 
-Sirisatri 424-5 :function equal divide ,font end work and gui
-Jinjuthakan 606-9:หน้า user and presentation slide
+1. Member contribution: Who does what and for how long.
+Thanyalak 162-9 :login page and source information from firebase .
+Sirisatri 424-5 :function equal divide ,font end work and gui.
+Jinjuthakan 606-9:user page and presentation slide.
 
-2. The best features of the game: the features that make the game fun or good
-It 's 
-3. The features that you would like to improve
-4. Other comments or suggestions
+2. The best features of the web app: the features that make the game fun or good.
+It 's equal divide feature.
+3. The features that you would like to improve.
+It 's not equal divide feature.
+4. Other comments or suggestions.
+It will can source data from firebase and display this information on some page.
--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,24 +1,14 @@
-// const authSwitchLinks = document.querySelectorAll('.switch');
-// const authModals = document.querySelectorAll('.auth .modal');
-// const authWrapper = document.querySelector('.auth');
-// const registerForm = document.querySelector('.register');
-// const loginForm = document.querySelector('.login');
 const loginFormWithGmail = document.querySelector('#loginGmail');
-
 var provider = new firebase.auth.GoogleAuthProvider();
-
-// console.log('signout>>>',signOut)
-// const firebase = require('firebase')
-
-
 
 // toggle auth modals
 
 const loginWithGmail = () => {
+  // console.log(provider);
   firebase.auth()
   .signInWithPopup(provider)
   .then((result) => { 
-    window.location = '../pages/newCostsplit.html'   
+    window.location = 'newCostsplit.html'   
   }).catch((error) => {
     // Handle Errors here.
     var errorCode = error.code;
@@ -30,55 +20,18 @@ const loginWithGmail = () => {
     // ...
   });
 }
-
-
-// authSwitchLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     authModals.forEach(modal => modal.classList.toggle('active'));
-//   });
-// });
-
-// register form
-// registerForm.addEventListener('submit', (e) => {
-// e.preventDefault();
-// const email = registerForm.email.value;
-// const password = registerForm.password.value;
-// firebase.auth().createUserWithEmailAndPassword(email,password)
-// .then(user => {
-//     console.log('registered', user);
-//     registerForm.reset();
-// })
-// .catch((error) => {
-//     registerForm.querySelector('.error').textContent = error.message;
-// });
-// });
-
-// login form
-// loginForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
-  
-//   const email = loginForm.email.value;
-//   const password = loginForm.password.value;
-//   firebase.auth().signInWithEmailAndPassword(email, password)
-//     .then(user => {
-//       console.log('logged in', user);
-//       loginForm.reset();
-//     })
-//     .catch(error => {
-//       loginForm.querySelector('.error').textContent = error.message;
-//     });
-// });
-
 // login form with gmail
 loginFormWithGmail.addEventListener('click', loginWithGmail);
-
-
 
 // auth listener
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
-    window.location = '../pages/newCostsplit.html'
+    
+    window.location = 'newCostsplit.html'
+    
+
   } else {
-    console.log('is not log in')
+    console.log('is not log in');
+    
   }
 });
